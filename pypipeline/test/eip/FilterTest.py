6fd62009dfb533cc66fb0a671a14d8a7276406d9
Vaibhav <vaibhavsinh@gmail.com>
2016-04-25 00:48:52 +0530

    Add class based filter as example

--- pypipeline/test/eip/FilterTest.py
+++ pypipeline/test/eip/FilterTest.py
@@ -10,11 +10,17 @@ class FilterTest(unittest.TestCase):
     def test_simple_pipeline(self):
         plumber = Plumber()
         builder1 = DslPipelineBuilder()
-        pipeline1 = builder1.source({"endpoint": Timer, "period": 1.0}).filter(filter_method).process(lambda ex: print(ex.in_msg.body))
+        pipeline1 = builder1.source({"endpoint": Timer, "period": 1.0}).filter(Filter()).process(lambda ex: print(ex.in_msg.body))
         plumber.add_pipeline(pipeline1)
         plumber.start()
         time.sleep(10)
         plumber.stop()
+
+
+class Filter:
+    def __call__(self, exchange):
+        parts = exchange.in_msg.body.split()
+        return int(parts[-1]) % 2 == 0
 
 
 def filter_method(exchange):
