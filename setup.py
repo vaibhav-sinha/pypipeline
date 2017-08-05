c9b898b8afedede07e2c937d3a8792253ef7d0d5
Vaibhav Sinha <vaibhavsinh@gmail.com>
2017-08-05 20:45:07 +0530

    Preparing for publishing

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name = "pypipeline",
+    name = "pypipeline-esb",
     packages = ["pypipeline"],
-    version = "0.0.1",
+    version = "1.0.0",
     description = "ESB for Python",
     author = "Vaibhav Sinha",
     author_email = "vaibhavsinh@gmail.com",
-    url = "http://pypipeline.feedparser.org/",
-    download_url = "http://pypipeline.feedparser.org/download/python3-pypipeline-1.0.1.tgz",
+    url = "https://github.com/vaibhav-sinha/pypipeline",
     keywords = ["esb", "eip", "pipeline"],
     classifiers = [
         "Programming Language :: Python",
@@ -19,26 +18,9 @@ setup(
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Text Processing :: Linguistic",
         ],
     long_description = """\
-Universal character encoding detector
--------------------------------------
-
-Detects
- - ASCII, UTF-8, UTF-16 (2 variants), UTF-32 (4 variants)
- - Big5, GB2312, EUC-TW, HZ-GB-2312, ISO-2022-CN (Traditional and Simplified Chinese)
- - EUC-JP, SHIFT_JIS, ISO-2022-JP (Japanese)
- - EUC-KR, ISO-2022-KR (Korean)
- - KOI8-R, MacCyrillic, IBM855, IBM866, ISO-8859-5, windows-1251 (Cyrillic)
- - ISO-8859-2, windows-1250 (Hungarian)
- - ISO-8859-5, windows-1251 (Bulgarian)
- - windows-1252 (English)
- - ISO-8859-7, windows-1253 (Greek)
- - ISO-8859-8, windows-1255 (Visual and Logical Hebrew)
- - TIS-620 (Thai)
-
-This version requires Python 3 or later; a Python 2 version is available separately.
+PyPipeline is meant to be a lightweight ESB, configurable via an intutive DSL. PyPipeline implements many of the Enterprise Integration Patterns.
 """,
     install_requires=[
     ],
